85bb8093383f443f6cdf699eb4e27b2c28e22991
Spiegel <spiegel.im.spiegel@gmail.com>
2022-08-06 20:59:59 +0900

    Upgrade github.com/google/licenseclassifier/v2

--- go.mod
+++ go.mod
@@ -7,15 +7,16 @@ require (
 	github.com/emicklei/dot v1.0.0
 	github.com/goark/errs v1.1.0
 	github.com/goark/gocli v0.12.0
-	github.com/google/licenseclassifier v0.0.0-20210722185704-3043a050f148
+	github.com/google/licenseclassifier/v2 v2.0.0-pre6
 	github.com/spf13/cobra v1.5.0
 	golang.org/x/tools v0.1.12
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/sergi/go-diff v1.0.0 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/sys v0.0.0-20220804214406-8e32c043e418 // indirect
 )
--- modules/licenses.go
+++ modules/licenses.go
@@ -5,17 +5,17 @@ import (
 	"path/filepath"
 	"regexp"
 
-	"github.com/google/licenseclassifier"
-	"github.com/google/licenseclassifier/stringclassifier"
+	licenseclassifier "github.com/google/licenseclassifier/v2"
+	"github.com/google/licenseclassifier/v2/assets"
 )
 
 var (
 	licenseRegexp = regexp.MustCompile(`^(?i)(LICEN(S|C)E|COPYING|README|NOTICE)(\..+)?$`)
-	classifier    *licenseclassifier.License
+	classifier    *licenseclassifier.Classifier
 )
 
 func init() {
-	classifier, _ = licenseclassifier.New(licenseclassifier.DefaultConfidenceThreshold)
+	classifier, _ = assets.DefaultClassifier()
 }
 
 func FindLicense(dir string) string {
@@ -27,16 +27,8 @@ func FindLicense(dir string) string {
 		if licenseRegexp.MatchString(f.Name()) {
 			path := filepath.Join(dir, f.Name())
 			if content, err := os.ReadFile(path); err == nil {
-				matches := func() stringclassifier.Matches {
-					defer func() {
-						if r := recover(); r != nil {
-							return
-						}
-					}()
-					return classifier.MultipleMatch(string(content), true)
-				}()
-				if len(matches) > 0 {
-					return matches[0].Name
+				if res := classifier.Match(content); len(res.Matches) > 0 {
+					return res.Matches[0].Name
 				}
 			}
 		}
@@ -44,7 +36,7 @@ func FindLicense(dir string) string {
 	return ""
 }
 
-/* Copyright 2021 Spiegel
+/* Copyright 2021-2022 Spiegel
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
